091a0c7741b45b19b9c25451eae60eb20a37cfb7
Taruu <markov2002@bk.ru>
2020-04-27 20:21:00 +0300

    req test

---
+++ req.py
@@ -0,0 +1,20 @@
+import socket
+import sys
+import time
+
+SERVER_HOST = 'localhost'
+SERVER_PORT = 40404
+
+message = [b'first message', b'second message']
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((SERVER_HOST, SERVER_PORT))
+
+for line in message:
+    time.sleep(0.3)
+    sock.send(line)
+    data = sock.recv(1024)
+    print('Client received:', repr(data))
+
+time.sleep(2)
+sock.close()
