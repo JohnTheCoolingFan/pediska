47b9fd565a3176e571332b630e8669c2428de5e9
JohnTheCoolingFan <ivan8215145640@gmail.com>
2020-04-27 20:40:40 +0300

    Sockets

--- daemon.py
+++ daemon.py
@@ -3,8 +3,24 @@
 import pediscord
 import getpass
 import os
+import asyncio
 
-client = pediscord.Client()
+class NewMessageServer(asyncio.Protocol):
+    def connection_made(self, transport):
+        peername = transport.get_extra_info('peername')
+        print('Connection from {}'.format(peername))
+        self.transport = transport
+
+    def data_received(self, data):
+        print('Received data: {}'.format(data.decode()))
+        self.transport.write(data)
+
+        self.transport.close()
+
+loop = asyncio.get_event_loop()
+coro = loop.create_server(NewMessageServer, '127.0.0.1', 40404)
+server = loop.run_until_complete(coro)
+print('Started on {}'.format(server.sockets[0].getsockname()))
 
 token = ''
 if os.path.exists('token.txt'):
@@ -14,6 +30,8 @@ else:
     token = getpass.getpass('Enter your token: ')
     with open('token.txt', 'w') as tokenfile:
         tokenfile.write(token)
+
+client = pediscord.Client(loop=loop)
 
 client.run(token, bot=False)
 
