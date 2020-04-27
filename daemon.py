89cf9548d3db10dcc2dc56685b680534dbd80043
JohnTheCoolingFan <ivan8215145640@gmail.com>
2020-04-27 19:15:22 +0300

    Client draft

--- daemon.py
+++ daemon.py
@@ -1,3 +1,20 @@
 #! /usr/bin/python
 
+import pediscord
+import getpass
+import os
+
+client = pediscord.Client()
+
+token = ''
+if os.path.exists('token.txt'):
+    with open('token.txt', 'r') as token_file:
+        token = token_file.read().rstrip()
+else:
+    token = getpass.getpass('Enter your token: ')
+    with open('token.txt', 'w') as tokenfile:
+        tokenfile.write(token)
+
+client.run(token, bot=False)
+
 print('This is daemon')
