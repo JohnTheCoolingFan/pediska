7053ddb3a06672adb7b6c26fc5fd23189151818e
JohnTheCoolingFan <ivan8215145640@gmail.com>
2020-04-28 00:02:56 +0300

    More comments, less garbage

--- daemon.py
+++ daemon.py
@@ -5,6 +5,7 @@ import getpass
 import os
 import asyncio
 
+# ...
 class NewMessageServer(asyncio.Protocol):
     def connection_made(self, transport):
         peername = transport.get_extra_info('peername')
@@ -20,6 +21,7 @@ coro = loop.create_server(NewMessageServer, '127.0.0.1', 40404)
 server = loop.run_until_complete(coro)
 print('Started on {}'.format(server.sockets[0].getsockname()))
 
+# Retrieve token
 token = ''
 if os.path.exists('token.txt'):
     with open('token.txt', 'r') as token_file:
@@ -29,12 +31,12 @@ else:
     with open('token.txt', 'w') as tokenfile:
         tokenfile.write(token)
 
+# pediscord client initialization
 client = pediscord.Client(loop=loop)
 
+# Try to send new messages to client (kinda)
 @client.event
 async def on_message(message: pediscord.Message):
     print(message.content)
 
 client.run(token, bot=False)
-
-print('This is daemon')
