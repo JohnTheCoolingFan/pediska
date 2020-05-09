0b492ba347eef07d4c2c1ad70a09f61e80e859c5
unknown <tk.andr100@gmail.com>
2020-05-09 20:19:13 +0200

    More info about guild

--- daemon.py
+++ daemon.py
@@ -44,7 +44,31 @@ async def on_ready():
     print("=" * len(startup))
     guilds_json = []
     for guild in client.guilds:
-        guild_list.append({"name":guild.name, "id":guild.id, "icon":guild.icon, "description":guild.description})
+        guild_list.append({"name":guild.name, "id":guild.id, "icon":guild.icon, "description":guild.description, "larfe_guild":guild.large})
+        guild_channels = []
+        for channel in guild.text_channels:
+            guild_channels.append({
+                "name":channel.name, "id":channel.id,
+                "topic":channel.topic, "slowmode_delay":channel.slowmode_delay,
+                "nsfw":channel.nsfw(), "news":channel.is_news(),
+                "members":channel.members, "type":channel.type,
+                "category":channel.category, "pos":channel.position
+            })
+        for channel in guild.voice_channels:
+            guild_channels.append({
+                "name":channel.name, "id":channel.id,
+                "user_limit":channel.user_limit, "members":channel.members,
+                "type":channel.type, "category":channel.category,
+                "pos":channel.position
+            })
+        for category in guild.categories:
+            guild_channels.append({
+                "name":category.name, "id":channel.id,
+                "nsfw":category.nsfw(),
+                "txt_ch":category.text_channels, "voice_ch":category.voice_channels,
+                "type":category.type, "pos":category.position
+            })
+        guild_list[len(guild_list) - 1]["channels"] = guild_channels
     guilds_json = json.dumps(guilds_json)
 
 @client.event
