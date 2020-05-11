0111c6a6e350432773e9b0f0e01dd8138194a73c
Taruu <markov2002@bk.ru>
2020-05-11 22:43:20 +0300

    add main.ui and test_client

---
+++ test_client.py
@@ -0,0 +1,18 @@
+from PyQt5 import QtWidgets, uic
+import sys
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('clientui/main.ui', self)
+        self.button = self.findChild(QtWidgets.QPushButton, 'pushButton')
+        self.button.clicked.connect(self.printButtonPressed)
+
+        self.show()
+
+    def printButtonPressed(self):
+        # This is executed when the button is pressed
+        print('printButtonPressed')
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
